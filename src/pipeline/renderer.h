8fe5d159c23e25011b07a201a672c01d8e8587d1
Julia Coll <julia.coll03@estudiant.upf.edu>
2023-05-18 16:47:40 +0200

    bufferToShader

--- src/pipeline/renderer.h
+++ src/pipeline/renderer.h
@@ -123,6 +123,7 @@ namespace SCN {
 
 		void cameraToShader(Camera* camera, GFX::Shader* shader); //sends camera uniforms to shader
 		void lightToShader(LightEntity* light, GFX::Shader* shader); //sends light uniforms to shader
+		void bufferToShader(GFX::Shader* shader);
 
 		void storeDrawCall(SCN::Node* node, Camera* camera);
 
